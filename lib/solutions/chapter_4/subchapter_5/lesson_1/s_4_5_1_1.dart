e3fd2198dbeb0ca3f85e9e957280233dd2b51988
Linda Sixtus <linda.sixtus@gmx.de>
2025-07-10 11:51:25 +0200

    Solution 4.5.1.1

--- lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
@@ -4,6 +4,10 @@ class S4511 extends StatelessWidget {
   const S4511({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return hello();
   }
+}
+
+StatelessWidget hello() {
+  return const Text('Hello World');
 }
