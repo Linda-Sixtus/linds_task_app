cd8503a958941a8730e9160cf1034076a8bc2333
Linda Sixtus <linda.sixtus@gmx.de>
2025-07-10 12:51:27 +0200

    Solution_Aufgabe_s_4_5_2_2_Linda

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,25 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("Hello Linda"),
+        const SizedBox(height: 20),
+        ElevatedButton(
+         child: Text("Klick mich"),
+         onPressed: () {
+           // To do
+         },
+        ),
+      ]);
   }
 }
