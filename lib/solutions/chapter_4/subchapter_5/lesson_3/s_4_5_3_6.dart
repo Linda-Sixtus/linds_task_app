6258c892453be25c4990c78fb76934ddba06f759
Linda Sixtus <linda.sixtus@gmx.de>
2025-07-14 15:22:20 +0200

    Solution_Linda_4_5_3_6_Iconlist

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -4,6 +4,33 @@ class S4536 extends StatelessWidget {
   const S4536({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        for (Product p in products)
+          ListTile(
+            leading: Icon(
+              Icons.shopping_cart
+            ),
+            title: Text(p.name),
+            subtitle: Text('\$${p.price.toStringAsFixed(2)}'),
+          ),
+      ],
+    );
   }
 }
+
+class Product {
+  final String name;
+  final double price;
+  Product(this.name, this.price);
+}
+
+List<Product> products = [
+  Product('Apple', 1.0),
+  Product('Banana', 0.5),
+  Product('Orange', 0.8),
+  Product('Peach', 3.2),
+  Product('Strawberry', 6.2),
+];
+
